8459003d72745ab66bd86d5d85dc81a8073e7bc9
Apodaca <eduardo1ja99@gmail.com>
2022-11-29 22:32:54 -0600

    feat: Refactor LoginUserUseCase

--- app/src/main/java/com/apodaca/loginapplication/usecase/LoginUserUseCase.kt
+++ app/src/main/java/com/apodaca/loginapplication/usecase/LoginUserUseCase.kt
@@ -1,36 +1,26 @@
 package com.apodaca.loginapplication.usecase
 
-import com.apodaca.loginapplication.model.domain.User
-import com.apodaca.loginapplication.model.db.dao.UserDao
-import java.lang.IllegalArgumentException
+import timber.log.Timber
 import javax.inject.Inject
 
 class LoginUserUseCase @Inject constructor(
-    private val userDao: UserDao
+    private val getUserByEmailUseCase: GetUserByEmailUseCase,
+    private val addLoggedInEmailToDataStoreUseCase: AddLoggedInEmailToDataStoreUseCase
 ) {
 
-    suspend operator fun invoke(email: String, password: String): Result {
-        return try {
-            val userDto = userDao.findByEmail(email)
+    suspend operator fun invoke(email: String, password: String) {
+        Timber.d("invoke: $email")
+        try {
+            val userDto = getUserByEmailUseCase(email)
             if (userDto.password != password) {
-                throw IllegalArgumentException("LoginUserUseCase: failed, passwords do not match")
+                Timber.e("LoginUserUseCase: failed, passwords do not match")
             }
-            Result.Success(User(userDto.email))
-        } catch (e: IllegalArgumentException) {
-            Result.Failure(FailureReason.INVALID_PASSWORD)
+            addLoggedInEmailToDataStoreUseCase(email)
         } catch (e: Exception) {
-            Result.Failure(FailureReason.USER_NOT_FOUND)
+            Timber.e("LoginUserUseCase: failed, exception: ${e.message}")
         }
 
     }
 
-    sealed class Result {
-        data class Success(val user: User) : Result()
-        data class Failure(val failureResult: FailureReason) : Result()
-    }
 
-    enum class FailureReason {
-        USER_NOT_FOUND,
-        INVALID_PASSWORD
-    }
 }
