8b312e89fc022037826a5306ac204392c2898277
Apodaca <eduardo1ja99@gmail.com>
2022-11-30 21:35:05 -0600

    feat: Color theme

---
+++ app/src/main/java/com/apodaca/loginapplication/Activity.kt
@@ -0,0 +1,12 @@
+package com.apodaca.loginapplication
+
+
+import androidx.appcompat.app.AppCompatActivity
+
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class Activity : AppCompatActivity() {
+
+}
