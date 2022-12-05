efc713a63c2aed688f6e889ca368bc27b922df23
Apodaca <eduardo1ja99@gmail.com>
2022-12-05 08:38:29 -0600

    feat: Logout use case

--- app/src/main/java/com/apodaca/loginapplication/screens/loggedin/LoggedInFragment.kt
+++ app/src/main/java/com/apodaca/loginapplication/screens/loggedin/LoggedInFragment.kt
@@ -1,7 +1,43 @@
 package com.apodaca.loginapplication.screens.loggedin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.apodaca.loginapplication.R
+import com.apodaca.loginapplication.databinding.FragmentLoggedInBinding
+import com.apodaca.loginapplication.databinding.FragmentLoginBinding
+import com.apodaca.loginapplication.screens.login.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoggedInFragment: Fragment(R.layout.fragment_logged_in) {
+
+@AndroidEntryPoint
+class LoggedInFragment : Fragment(R.layout.fragment_logged_in) {
+    private val viewModel: LoggedInViewModel by viewModels()
+    private var _binding: FragmentLoggedInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentLoggedInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.logOut -> {
+                    viewModel.logOutClicked()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 }
